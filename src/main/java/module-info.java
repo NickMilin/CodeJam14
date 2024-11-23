a290c023e90d21c202451ebd022b651be80dbbbe
Mathieu <matpestel@gmail.com>
2024-11-23 14:26:27 -0500

    Update module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,7 +3,6 @@ module ca.mcjammers.codejam14 {
     requires javafx.fxml;
     requires java.net.http;
     requires com.fasterxml.jackson.databind;
-    requires json.simple;
 
 
     opens ca.mcjammers.codejam14 to javafx.fxml;
